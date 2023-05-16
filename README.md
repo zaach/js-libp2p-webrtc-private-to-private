ebf487a76c2dfed2b66dca5b55f6dae3d8ba4ce0
Zachary Carter <zack.carter@gmail.com>
2023-05-16 00:41:41 -0700

    add updated dependencies and @zaach/rollup-plugin-node-polyfills

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-Debug repo for js-libp2p-webrtc example: https://github.com/libp2p/js-libp2p-webrtc/tree/main/examples/browser-to-browser
+This repo was created to debug the browser-to-browser example from js-libp2p-webrtc: https://github.com/libp2p/js-libp2p-webrtc/tree/main/examples/browser-to-browser
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,23 @@
-export default {
+import rollupNodePolyFill from "@zaach/rollup-plugin-node-polyfills";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      events: "@zaach/rollup-plugin-node-polyfills/polyfills/events",
+    },
+  },
   build: {
-    target: 'es2022'
+    target: "es2022",
   },
   optimizeDeps: {
-    esbuildOptions: { target: 'es2022', supported: { bigint: true } }
+    esbuildOptions: { target: "es2022", supported: { bigint: true } },
   },
   server: {
-    open: true
-  }
-}
+    rollupOptions: {
+      plugins: [rollupNodePolyFill()],
+    },
+    open: true,
+  },
+});
+
